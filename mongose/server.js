ecea42cb1963828ad3a6a1622983115c5689a5e9
MOStafa <lordtata2014@gmail.com>
2020-08-23 21:45:41 +0200

    q

--- mongose/server.js
+++ mongose/server.js
@@ -28,7 +28,7 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/xyz',express.static(path.join(__dirname,'xyz')));
 
-mongoose.connect("mongodb://localhost:27017/test",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://mostafa:mostafa@cluster0.uzvic.mongodb.net/test",{useNewUrlParser:true,useUnifiedTopology:true});
 
 process.env.PORT
 app.listen(process.env.PORT||3000,()=>{
